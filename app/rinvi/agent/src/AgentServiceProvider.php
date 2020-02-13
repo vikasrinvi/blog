cfc73068e296dc98bec0b15d35b773003246fc61
vikasrinvi <vikasmrnv@gmail.com>
2020-02-13 06:23:59 +0530

    service

--- app/rinvi/agent/src/AgentServiceProvider.php
+++ app/rinvi/agent/src/AgentServiceProvider.php
@@ -16,6 +16,12 @@ class AgentServiceProvider extends ServiceProvider
     /**
      * Register the service provider.
      */
+    public function boot()
+    {
+
+        $this->publishMigrations();
+    }
+
     public function register()
     {
         $this->app->singleton('agent', function ($app) {
@@ -33,5 +39,16 @@ class AgentServiceProvider extends ServiceProvider
     public function provides()
     {
         return ['agent', Agent::class];
+    }
+
+    protected function publishMigrations()
+    {
+        if (!class_exists('CreateGSuiteConfigurationsTable')) {
+            $timestamp = date('Y_m_d_His', time());
+
+            $this->publishes([
+                __DIR__ . '/../database/migrations/tenant/create_g_suite_configurations_table.php.stub' => $this->app->databasePath() . "/migrations/tenant/{$timestamp}_create_g_suite_configurations_table.php",
+            ]);
+        }
     }
 }
